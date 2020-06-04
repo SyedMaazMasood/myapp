abd32a43dc749f9bb1901441e33d978172572c18
SyedMaazMasood <syed.maaz.masood@gmail.com>
2020-06-04 20:59:00 +0500

    commit on master

--- src/components/AppWithRouter.js
+++ src/components/AppWithRouter.js
@@ -1,9 +1,14 @@
 import React from "react";
 import createHistory from "history/createBrowserHistory";
 import { Router, Route, Switch, withRouter } from "react-router-dom";
+import DiscoverPage from "../containers/DiscoverPage";
+import FollowingPage from "../containers/FollowingPage";
 import HomePage from "../containers/HomePage";
 import LoginPage from "../containers/SignIn";
 import SignUp from "../containers/SignUp";
+import ProfilePage from "../containers/ProfilePage";
+import SettingsPage from "../containers/SettingsPage";
+import NotFound from "./NotFound";
 
 export const history = createHistory();
 
@@ -12,6 +17,11 @@ const Root = () => (
     <Route exact path="/" component={HomePage} />
     <Route path="/signup" component={SignUp} />
     <Route path="/login" component={LoginPage} />
+    <Route path="/profile/:id" component={ProfilePage} />
+    <Route path="/following" component={FollowingPage} />
+    <Route path="/discover" component={DiscoverPage} />
+    <Route path="/settings" component={SettingsPage} />
+    <Route component={NotFound} />
   </Switch>
 );
 
